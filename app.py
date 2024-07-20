27963d25d5a69644050d95808eb5d4e24fbf2275
DangMinh14 <minhdangtank1@gmail.com>
2024-07-20 18:37:57 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề của ứng dụng
-st.title('Hiển thị hình ảnh từ URL')
+st.title('Đây là test')
 
 # URL hình ảnh mẫu
 image_url = "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
